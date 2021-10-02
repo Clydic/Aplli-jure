5fe33e7116471f4cf1897d4e1d0ad9632cbdefc0
cedric <cedric.du.50@gmail.com>
2021-10-02 02:10:49 +0200

    création du menu, à mettre certainement dans une autre vue

--- Application/vues/view_header.php
+++ Application/vues/view_header.php
@@ -13,5 +13,27 @@
 		<img class=" img-fluid w-50 h-50" src="<?php echo $logo; ?>" alt="Le logo de l'AFPA">
 		<h1 class="  m-auto w-75 text-center"><?php echo $h1Title; ?> </h1>
 	</header>
-		<main class="bg-light container pt-2 h-50 mt-3">
-
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Session de formation</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Archives</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Liste des jurés</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Gestion de formation</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
