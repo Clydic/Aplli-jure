e9d43f7e6490b134942defc9696ca799c8dabdca
Cédric Paimblanc <cedric.du.50@gmail.com>
2021-10-10 22:11:05 +0200

    requête affichachage liste session d'examen

--- Application/modele/modele.inc.php
+++ Application/modele/modele.inc.php
@@ -1,3 +1,26 @@
 <?php // modele/modele.inc.php
-	function
+	
+	require("classes/CRMJure.class.php");
+	$connection= CRMJures::getConnection();
+	function getListExam($connect)
+	{	
+		$sql="SELECT f.Intitule_de_formation,
+		       	s.IDSessionFormation, 
+			e.DateSessionExam 
+			from Formation f
+			JOIN Session_Formation s on s.IDFormation = f.IDFormation
+			JOIN SessionExamen e on e.IDSessionExam = s.IDSessionExam";
+		$cursor = $connect->query($sql);
+		$result = $cursor->fetchAll();
+		
+		return $result;	
+	}
+$affiche =getListExam($connection);
+$message = ""
+foreach($affiche as $line)
+{
+	$message.="<tr><td>".$line["Intitule_de_formation"].
+		$line["IDSessionFormation"]."</td>";
+	$message.="<td>".$line["DateSessionExam"]."</td>"
+}
 ?>
---
+++ Application/test/test.php
@@ -0,0 +1,5 @@
+<?php // test/test.php 
+	require ("classes/SessionExamen.class.php");
+		$examen1 = new SessionExamen(2,"2019-03-02");
+		echo $examen1;	
+?>
