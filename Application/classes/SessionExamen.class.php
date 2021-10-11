71026299355dd014480adf304d95e974124b5c13
Clydic <70671047+Clydic@users.noreply.github.com>
2021-10-11 08:23:51 +0200

    correction

--- Application/classes/SessionExamen.class.php
+++ Application/classes/SessionExamen.class.php
@@ -63,8 +63,8 @@
 		 * */
 		public function setDateExamen(string $dateToSet)
 		{	
-			$exp="/^\d{4}\-\d{2}\-\d{2}$/";
-			if(preg_match($exp))
+			$exp="/^\d{4}\-\d{2}\-\d{2}/";
+			if(preg_match($exp,$dateToSet))
 			{
 				$this->date=$dateToSet;
 			}			
