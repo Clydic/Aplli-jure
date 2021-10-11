e07d8c875f88b5069537a23117d9d1d3f4ea8d9f
PaulBriand <briand.paul@outlook.fr>
2021-10-11 08:32:24 +0200

    Lien vers ListExam

--- Application/vues/view_connexionForm.php
+++ Application/vues/view_connexionForm.php
@@ -3,9 +3,10 @@
         <h1 class="text-center">Se connecter en tant que Formateur</h1>
     </div>
     <div id="divConnexionFormateur" class="row align-self-center text-center col-md-6">
-        <form action="" method="GET">
-            <input type="text" name="user" placeholder="Votre nom d'utilisateur"><br><br>
-            <input type="password" name="password" placeholder="Votre mot de passe"><br><br>
+        <form action="index.php" method="GET">
+            <!-- <input type="text" name="user" placeholder="Votre nom d'utilisateur"><br><br>
+            <input type="password" name="password" placeholder="Votre mot de passe"><br><br> -->
+            <input type="hidden" name="action" value="listExam">
             <input type="Submit" value="Se Connecter" class="text-center">
         </div>
     </div>
