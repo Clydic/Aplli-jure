a90833bf92e7683a53e4ec40734aab8adc3f7760
Clydic <70671047+Clydic@users.noreply.github.com>
2021-10-15 15:57:35 +0200

    Ajout des icones et correction vu list examen

--- Application/vues/view_header.php
+++ Application/vues/view_header.php
@@ -5,13 +5,17 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" href="assets/CSS/fontawesome.css"> 
+		<link rel="stylesheet" href="assets/CSS/all.min.css">
+		<!-- <link rel="stylesheet" href="assets/CSS/solid.css"> -->
 		<link rel="stylesheet" href="assets/CSS/theme/Minty.css">
 		<link rel="stylesheet" href="assets/CSS/style.css">
 		<title><?php echo $tabTitle; ?>  </title>
 	</head>
-	<body class="bg-primary">
-		<div class="container-sm my-auto" id="wrapper">
-			<header class="d-flex  bg-light mb-1 border border-3">
-				<img class=" img-fluid " src="<?php echo $logo; ?>" alt="Le logo de l'AFPA">
-				<h1 class="  m-auto  text-center"><?php echo $h1Title; ?> </h1>
+	<body >
+		<div class="bg-center " id="wrapper">
+			<header class="d-flex  flex-column flex-md-row flex-md-nowrap bg-light mb-1 border border-3">
+				<img class=" img-fluid column-6" src="<?php echo $logo; ?>" alt="Le logo de l'AFPA">
+				<h1 class="  m-auto  text-center column-6"><?php echo $h1Title; ?> </h1>
 			</header>
--- Application/vues/view_list-exam.php
+++ Application/vues/view_list-exam.php
@@ -1,12 +1,16 @@
 <!--vues/view_list-exam.php-->
 	<main class=" bg-light  pt-2 mt-3">
 		<h2 class=" text-center">Liste des sessions d'examen</h2>
-		<div class="overflow-auto h-50">
+		<div class="overflow-auto h-25">
 
 			<table class=" container col-12  mt-5  
-			fs-4 table table-striped table-secondary mb-3">
+			fs-4 table table-striped table-dark mb-3">
 <?php echo $listExamen;?>
 			</table>
+		</div>
+		<div class="d-flex jusitfy-content-center mt-1">
+
+			<a class="btn btn-info m-auto" href="#" role="button">Ajouter</a>
 		</div>	
 	</main>
 
