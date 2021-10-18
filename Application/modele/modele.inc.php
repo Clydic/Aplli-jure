fb71671d719616010a490f0819abddeb1c5babed
PaulBriand <briand.paul@outlook.fr>
2021-10-18 10:36:35 +0200

    Ajout Connexion

--- Application/modele/modele.inc.php
+++ Application/modele/modele.inc.php
@@ -1,6 +1,7 @@
 <?php // modele/modele.inc.php
 	
 	require("classes/CRMJure.class.php");
+
 	$connection= CRMJures::getConnection();
 	function getListExam($connect)
 	{	
@@ -30,5 +31,37 @@
 			
 		}
 		return $message;	
+	}
+
+	function getConnectAdmin($connect,$user,$pass) : bool
+	{	
+		$sql="SELECT identifiant, Mot_de_passe FROM utilisateur WHERE TypeUtilisateur = \"Administrateur\"";
+		$cursor = $connect->query($sql);
+		$result = $cursor->fetchAll();
+		
+		foreach($result as $line)
+		{
+			if($line['identifiant'] == $user && $line['Mot_de_passe'] == $pass)
+			{
+				return true;
+			}
+		}	
+		return false;
+	}
+
+	function getConnectForm($connect,$user,$pass) : bool
+	{	
+		$sql="SELECT identifiant, Mot_de_passe FROM utilisateur WHERE TypeUtilisateur = \"Formateur\"";
+		$cursor = $connect->query($sql);
+		$result = $cursor->fetchAll();
+		
+		foreach($result as $line)
+		{
+			if($line['identifiant'] == $user && $line['Mot_de_passe'] == $pass)
+			{
+				return true;
+			}
+		}	
+		return false;
 	}
 ?>
