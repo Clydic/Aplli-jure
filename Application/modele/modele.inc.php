6a4d1fb02881595839acc4f29807bacacf06bb5c
Clydic <70671047+Clydic@users.noreply.github.com>
2021-10-18 13:48:58 +0200

    suppression fonction getlistexamen

--- Application/modele/modele.inc.php
+++ Application/modele/modele.inc.php
@@ -2,36 +2,6 @@
 	
 	require("classes/CRMJure.class.php");
 
-	$connection= CRMJures::getConnection();
-	function getListExam($connect)
-	{	
-		$sql="SELECT f.Intitule_de_formation,
-		       	s.IDSessionFormation, 
-			e.DateSessionExam 
-			from Formation f
-			JOIN Session_Formation s on s.IDFormation = f.IDFormation
-			JOIN SessionExamen e on e.IDSessionExam = s.IDSessionExam";
-		$cursor = $connect->query($sql);
-		$result = $cursor->fetchAll();
-		
-		$message = "";
-		foreach($result as $line)
-		{
-			$message.="<tr><td>".$line["Intitule_de_formation"].
-				$line["IDSessionFormation"]."</td>";
-			$message.="<td>".$line["DateSessionExam"]."</td>".
-				"<td>
-					<a href=\"\"><i class=\"fas fa-info-circle\"></i></a>
-				</td>
-				<td>
-					<a href=\"\"><i class=\"fas fa-minus-circle bg-danger\">
-					</i></a>
-				
-				</td>";
-			
-		}
-		return $message;	
-	}
 
 	function getConnectAdmin($connect,$user,$pass) : bool
 	{	
