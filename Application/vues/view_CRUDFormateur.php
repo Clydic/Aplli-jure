21fb85f0276a293229410a6f9d3b6b19001dc0a0
PaulBriand <briand.paul@outlook.fr>
2021-10-18 13:51:34 +0200

    CRUD

--- Application/vues/view_CRUDFormateur.php
+++ Application/vues/view_CRUDFormateur.php
@@ -1,3 +1,7 @@
 <main>
-    
+    <div class="overflow-auto h-50">
+        <table class=" container col-12  mt-5  fs-4 table table-striped table-dark mb-3">
+            <?php echo $listFormateur;?>
+        </table>
+    </div>
 </main>
