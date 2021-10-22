ed9818be15f6e346d3e76f40fd04d8586200b3cb
Clydic <70671047+Clydic@users.noreply.github.com>
2021-10-22 08:30:12 +0200

    fix : Correction du lien de gestion des jurés

--- Application/vues/view_accueilForm.php
+++ Application/vues/view_accueilForm.php
@@ -3,7 +3,7 @@
     <input type="submit" value="listExam">
 </form>
 <div class="d-flex flex-column justify-content-start container ">
-<a href="index.php?action=view_accueilForm" class="align-self-center"><img src="source/jure.png" alt="Un dessin de juré"></a>
-<a href="index.php?action=view_listExam" class="align-self-center">><img src="source/examen.png" alt="Un dessin pour les examens"></a>
+<a href="#" class="align-self-center"><img src="source/jure.png" alt="Un dessin de juré"></a>
+<a href="index.php?action=listExam" class="align-self-center">><img src="source/examen.png" alt="Un dessin pour les examens"></a>
 
 </div>
