d556c9bb5ff081040156bdf810323e1451608466
PaulBriand <briand.paul@outlook.fr>
2021-10-22 12:00:56 +0200

    Doc technique

--- Application/vues/view_connexionAdmin.php
+++ Application/vues/view_connexionAdmin.php
@@ -3,7 +3,7 @@
         <h1 class="text-center">Se connecter en tant qu'Administrateur</h1>
     </div>
     <div id="divConnexionFormateur" class="row align-self-center text-center col-md-6">
-        <form action="" method="GET">
+        <form action="" method="POST">
             <input type="text" name="user" placeholder="Votre nom d'utilisateur"><br><br>
             <input type="password" name="password" placeholder="Votre mot de passe"><br><br>
             <input type="hidden" name="action" value="accueilAdmin">
